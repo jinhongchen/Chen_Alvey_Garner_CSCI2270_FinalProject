36505790fafd66de166a6450bfb0a00a3c0f6b1f
Jual6332 <jual6332@colorado.edu>
2016-04-21 00:17:19 -0600

    Added files via upload with our changes
    1. Added a new services struct to .h file, and two new public methods
    2. Added a "Plan a Trip" option for the menu, and a separate menu for planning a trip, involving the two new methods
    3. Programmed in two new public methods for the verticesGraph.cpp file to add a service and print services.
    
    - If you have any questions about the changes, just ask via email.

---
+++ verticesGraph.h
@@ -0,0 +1,63 @@
+#ifndef VERTICESGRAPH_H
+#define VERTICESGRAPH_H
+#include <vector>
+#include <iostream>
+
+struct vertex;
+
+struct service;
+
+struct adjVertex
+{
+    vertex *v;
+    int weight;
+};
+
+struct service
+{
+    std::string name;
+    int cost;
+};
+
+struct vertex
+{
+    std::string name;
+    int district;
+    int distance;
+    int ID;
+    int position;
+    bool visited;
+    vertex *previous;
+    std::vector<adjVertex> adj;
+    std::vector<service> services;
+
+};
+
+class verticesGraph
+{
+    private:
+        std::vector<vertex> vertices;
+    public:
+        verticesGraph(char * fileName);
+        verticesGraph();
+        //~verticesGraph();
+        //array doesn't need to be destroyed?
+        void buildGraph(char * fileName);
+        void assignDistricts();
+        void BFTraversalLabel(std::string starting,int distID);
+        void displayEverything();
+        void shortestDistance(std::string starting,std::string destination);
+        void limitedDistancePath(std::string starting,std::string destination,int distance);
+        void vertexInformation(std::string v1);
+        void graphInformation();
+        void addVertex(std::string name);
+        void addEdge(std::string v1,std::string v2,int weight);
+        void snakesAndLadders();
+        void addService(std::string name,std::string service, int cost);
+        void displayServices();
+        int findDistance(std::string v1,std::string v2);
+        vertex * findVertex(std::string name);
+        vertex * Dijkstra(std::string starting,std::string destination);
+};
+
+#endif
